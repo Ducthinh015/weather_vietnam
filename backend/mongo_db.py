79f2394e9e6c1755f5004f47afbd88f6b141652f
THINH <158245952+Ducthinh015@users.noreply.github.com>
2025-11-26 13:26:36 +0700

    Update mongo_db.py

--- backend/mongo_db.py
+++ backend/mongo_db.py
@@ -1,4 +1,11 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient("mongodb://localhost:27017", serverSelectionTimeoutMS=10000, appname="AgriCastAI")
-db = client["agricast_ai"]
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb+srv://2331540234_db_user:0905175313@cluster0.qyfcbff.mongodb.net/agricast?retryWrites=true&w=majority"
+)
+
+client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=10000, appname="AgriCastAI")
+
+db = client["agricast"]
